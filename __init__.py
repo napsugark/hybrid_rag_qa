3c97264349cc5fc231b6aa06f6b933c145fa7cff
Napsugar Kelemen <napsugar.kelemen@qubiz.com>
2026-02-11 17:33:14 +0200

    refator: restructured code, deleted unnecessary files

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-App v2 package initialization
-"""
-
-from .app import HybridRAGApplication
-
-__version__ = "2.0.0"
-__all__ = ["HybridRAGApplication"]
--- components/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Custom component initialization
-"""
-
-from .metadata_enricher import MetadataEnricher
-from .summarizer import DocumentSummarizer
-from .query_metadata_extractor import RomanianQueryMetadataExtractor
-
-__all__ = ["MetadataEnricher", "DocumentSummarizer", "RomanianQueryMetadataExtractor"]
